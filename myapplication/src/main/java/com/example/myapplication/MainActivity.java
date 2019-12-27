4535a037b3b467478389f81194cacb259a11ab76
leesecre <59246292+leesecre@users.noreply.github.com>
2019-12-27 15:51:41 +0900

    let's git it

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,132 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.res.ResourcesCompat;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.ListView;
+import androidx.core.content.ContextCompat;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private Integer[] mThumbIds = { R.drawable.kr,R.drawable.kr2,R.drawable.kr3,
+            R.drawable.mx, R.drawable.mx2,R.drawable.mx3, R.drawable.fr,R.drawable.fr2,R.drawable.fr3,
+            R.drawable.pl,R.drawable.pl2,R.drawable.pl3, R.drawable.sa,R.drawable.sa2,R.drawable.sa3,
+            R.drawable.cn,R.drawable.cn2,R.drawable.cn3
+    };
+
+    DisplayMetrics mMetrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout linear = new LinearLayout(this);
+        TabHost host =(TabHost)findViewById(R.id.host);
+        host.setup();
 
-        Button bt = new Button(this);
+        TabHost.TabSpec spec = host.newTabSpec("tab1");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_icon1,null));
+        spec.setContent(R.id.listView);
+        host.addTab(spec);
 
-        bt.setText("Button 1");
-        linear.addView(bt);
-        // 232323233232232 뭐가 바뀐게있나요?
+        spec = host.newTabSpec("tab2");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_icon2,null));
+        spec.setContent(R.id.gridview);
+        host.addTab(spec);
 
-        Button bt2 = new Button(this);
-        bt2.setText("Button 2");
-        linear.addView(bt2);
+        spec = host.newTabSpec("tab3");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_icon3,null));
+        spec.setContent(R.id.tab_content3);
+        host.addTab(spec);
 
-        setContentView(linear);
+        mListView = (ListView)findViewById(R.id.listView);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(gridviewOnItemClickListener);
+
+        mMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+
+        /* 아이템 추가 및 어댑터 등록 */
+        dataSetting();
+
+
     }
+    private GridView.OnItemClickListener gridviewOnItemClickListener
+            = new GridView.OnItemClickListener() {
+
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                long arg3) {
+
+            Toast.makeText(MainActivity.this,
+                    arg0.getAdapter().getItem(arg2).toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private void dataSetting(){
+
+        ListAdapter mMyAdapter = new ListAdapter();
+
+
+        for (int i=0; i<30; i++) {
+            mMyAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon), "name_" + i, "contents_" + i);
+        }
+
+        /* 리스트뷰에 어댑터 등록 */
+        mListView.setAdapter(mMyAdapter);
+    }
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return mThumbIds[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            int rowWidth = (mMetrics.widthPixels) / 4;
+
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(rowWidth,rowWidth));
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imageView.setPadding(1, 1, 1, 1);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+    }
+
+
 }
+
